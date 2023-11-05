aac708f7af274f3ff03bd72a038f1a3ee07538ea
Anton Hempel <hempel.anton@gmail.com>
2023-11-05 18:32:55 +0100

    Implemented new operators for iterator class and fixed some bugs

--- src/urn_unstable.cpp
+++ src/urn_unstable.cpp
@@ -9,13 +9,13 @@
 #include <limits>
 using namespace std;
 
-using uint = unsigned int;
-using Draw = std::vector<uint>;
+//using uint = int; //unsigned int
+using Draw = std::vector<int>;
 
 string to_string(const Draw& draw)
 {   
     string stdraw {};
-    for(uint posCount{}; posCount < draw.size(); ++posCount)
+    for(int posCount{}; posCount < draw.size(); ++posCount)
     {
         stdraw += to_string(draw.at(posCount));
         if(posCount != (draw.size()-1))
@@ -33,69 +33,48 @@ class UrnOR
     class Iterator
     {   
         public:
-            using iterator_category = std::bidirectional_iterator_tag;//std::forward_iterator_tag; //std::bidirectional_iterator_tag; //erstmal bidirectional und dann später random access wenn möglich
+            using iterator_category = std::random_access_iterator_tag;  //std::bidirectional_iterator_tag;
             using difference_type   = std::ptrdiff_t;
             using value_type        = Draw; //vector<unsigned int>
             using pointer           = Draw*;  
             using reference         = const Draw; //Kommentar anmerken
+            using size_type         = std::size_t;  //https://en.cppreference.com/w/cpp/types/size_t
 
             //Iterators: constructible, copy-constructible, copy-assignable, destructible and swappable
             enum class Status
             {
-                begin,
+                invalidFront,
                 valid,
-                invalid,
-                end
+                invalidBack
             };
             
-            Iterator(const UrnOR* urn, const uint& ordinalnumber, const Status& status): m_urn{ urn },
+            Iterator(const UrnOR* urn, const int& ordinalnumber, const Status& status): m_urn{ urn },
                                                                                          m_ordinalnumber{ ordinalnumber },
                                                                                          m_status { status }{}
-
-            string stat()
-            {   
-                if(m_status == Status::begin)
-                {
-                    return "begin";
-                }
-                if(m_status == Status::end)
-                {
-                    return "end";
-                }
-                if(m_status == Status::valid)
-                {
-                    return "valid";
-                }
-                if(m_status == Status::invalid)
-                {
-                    return "invalid";
-                }
-                return "Fail";
-            }
-
-            uint n() const
+            
+            int n() const
             {
                 return (*m_urn).n();
             }
 
-            uint k() const
+            int k() const
             {
                 return (*m_urn).k();
             }
 
-            uint z() const
+            int z() const
             {
                 return (*m_urn).z();
             }
 
-            uint ordinalnumber() const
+            int ordinalnumber() const
             {
                 return m_ordinalnumber;
             }
 
             const value_type operator*() const 
             {   
-                if(m_ordinalnumber >= z() || m_ordinalnumber < 0)   //wird nicht kleiner als null da m_ordinalnumber ein uint ist
+                if(m_ordinalnumber >= z() || m_ordinalnumber < 0)
                 {
                     return Draw((k()), 0);
                 }
@@ -104,23 +83,19 @@ class UrnOR
 
             /*const*/ Iterator operator++() //Iterator&
             {   
-                if (m_status == Status::begin)
+                if (m_status == Status::invalidFront)
                 {
                     m_status = Status::valid;
                     m_ordinalnumber = 0;
                 }
-                else if(m_status != Status::end)
+                else if(m_status != Status::invalidBack)
                 {
                     ++m_ordinalnumber;
                     m_status = Status::valid;
                 }
-                if(m_ordinalnumber == z())  //(m_ordinalnumber >= z())
+                if(m_ordinalnumber >= z())
                 {
-                    m_status = Status::end;
-                }
-                else if(m_ordinalnumber > z())
-                {
-                    m_status = Status::invalid;
+                    m_status = Status::invalidBack;
                 }
                 return *this;
             }
@@ -134,29 +109,19 @@ class UrnOR
             
             /*const*/ Iterator operator--() //Iterator&
             {   
-                if(m_status == Status::end)
+                if(m_status == Status::invalidBack)
                 {
                     m_status = Status::valid;
-                    m_ordinalnumber = m_urn->z() - 1;   //Warum nicht z() - 1?
+                    m_ordinalnumber = z() - 1;    //m_urn->z() - 1;   //Warum nicht z() - 1?
                 }
-                else if(m_status != Status::begin)
+                else if(m_status != Status::invalidFront)
                 {   
                     --m_ordinalnumber;
                     m_status = Status::valid;
                 }
-                /*
-                if(m_ordinalnumber < 0) //wird nicht kleiner als null da m_ordinalnumber ein uint ist
-                {                       //wird auf uint max 4294967295 gesetzt
-                    m_status = Status::begin;
-                }
-                */
-                
-                //wann wird Status zu begin?? -> eins vor dem ersten Draw?
-                //d.h. wenn wir mit uint arbeiten wäre es dann max(uint) -> 4294967295 danach dann invalid
-                
-                if(m_ordinalnumber > z())       //nur eine Übergangslösung
-                {
-                    m_status = Status::begin;
+                if(m_ordinalnumber < 0)
+                {                     
+                    m_status = Status::invalidFront;
                 }
                 return *this;
             }
@@ -170,9 +135,9 @@ class UrnOR
             
             friend bool operator== (const Iterator& a, const Iterator& b) 
             { 
-                return (a.m_ordinalnumber == b.m_ordinalnumber) || 
-                    (a.m_status == Status::end && b.m_status == Status::end) ||
-                    (a.m_status == Status::begin && b.m_status == Status::begin); //wird mit der alten Version nicht aufgerufen
+                return (a.m_ordinalnumber == b.m_ordinalnumber);
+                    //(a.m_status == Status::end && b.m_status == Status::end) ||
+                    //(a.m_status == Status::begin && b.m_status == Status::begin); //wird mit der alten Version nicht aufgerufen
             }   
 
             friend bool operator!= (const Iterator& a, const Iterator& b)
@@ -180,12 +145,32 @@ class UrnOR
                 return !(a == b); 
             }
 
+            friend bool operator< (const Iterator& a, const Iterator& b)  
+            { 
+                return (a.m_ordinalnumber < b.m_ordinalnumber);
+            }
+
+            friend bool operator> (const Iterator& a, const Iterator& b)  
+            { 
+                return (a.m_ordinalnumber > b.m_ordinalnumber);
+            }
+
+            friend bool operator<=(const Iterator& a, const Iterator& b)
+            { 
+                return (a == b || a < b);
+            }
+
+            friend bool operator>=(const Iterator& a, const Iterator& b)
+            { 
+                return (a == b || a > b);
+            }
+
             //Addition mit += (bei allen additionen und subtraktionen Klammerausdrücke testen in catch2)
             /*const*/ Iterator& operator+=(const difference_type& other)
             {   
                 if(other >= 0)
                 {
-                    for(uint incCount{}; incCount < other; ++incCount)
+                    for(int incCount{}; incCount < other; ++incCount)
                     {
                         ++(*this);
                     }
@@ -193,13 +178,14 @@ class UrnOR
                 else if(other < 0)
                 {   
                     long int positivOther {other * -1};
-                    for(uint decCount{}; decCount < positivOther; ++decCount)
+                    for(int decCount{}; decCount < positivOther; ++decCount)
                     {
                         --(*this);
                     }
                 }
                 return *this;
             }
+
             //Addition mit +
             Iterator operator+(const difference_type& other) const //ist besser ohne const
             { 
@@ -223,44 +209,59 @@ class UrnOR
             }
 
             //Difference
-            //Funktioniert noch nicht ganz richtig für rbegin - rend wegen uint max
-            /*
             difference_type operator-(const Iterator& other)
             { 
                 return m_ordinalnumber - other.m_ordinalnumber;
             }
-            */
+
+            //Index
+            //it[0] = {2,2}; ist nicht möglich da reference = const Draw und so kann der Wert nicht verändert werden
+            //Sollte aber kein Problem sein, da es für die Urn nicht möglich sein sollte
+            reference operator[](size_type index) const 
+            { 
+                if(index >= z() || index < 0)
+                {
+                    return Draw((k()), 0);
+                }
+                return (*m_urn).draw(index);
+            }
+
+
             ~Iterator() = default;
 
         protected:
             const UrnOR* m_urn;
-            uint m_ordinalnumber;
+            int m_ordinalnumber;
             Status m_status;
     };
 
     public:
-        explicit UrnOR(uint n,uint k,uint check = 1):m_n { n },
-                                                     m_k { k },
-                                                     m_z { static_cast<uint>(pow(n,k)) }
-        {
+        explicit UrnOR(int n,int k,int check = 1):m_n { n },
+                                                  m_k { k },
+                                                  m_z { static_cast<int>(pow(n,k)) }
+        {   
+            if(m_n < 0 || m_k < 0)                                                    //Kommt neu dazu wegen uint --> int
+            {
+                throw std::domain_error("UrnOR with negative n or k is not valid.");
+            }
             if (check == 1 && m_n == 0 && m_k > 0)
             {
-                throw std::domain_error(
-                    "UrnOR with n == 0 and k > 0 is not valid.");
+                throw std::domain_error("UrnOR with n == 0 and k > 0 is not valid.");
             }
+            
         }
 
-        uint n() const
+        int n() const
         {
             return m_n;
         }
 
-        uint k() const
+        int k() const
         {
             return m_k;
         }
 
-        uint z() const
+        int z() const
         {
             return m_z;
         }
@@ -272,7 +273,7 @@ class UrnOR
 
         Iterator end()
         {
-            return Iterator(this,m_z,Iterator::Status::end);
+            return Iterator(this,m_z,Iterator::Status::invalidBack);
         }
 
         auto rbegin()
@@ -289,7 +290,7 @@ class UrnOR
         {   
             if(m_k == draw.size())
             {
-                for(uint posCount {}; posCount < m_k; ++posCount)
+                for(int posCount {}; posCount < m_k; ++posCount)
                 {   
                     if(!(draw.at(posCount) <= (m_n-1)))
                     {   
@@ -305,12 +306,12 @@ class UrnOR
         {   
             if(valid(draw))
             {        
-                for (uint downCount { m_k }; downCount > 0; --downCount)
+                for (int downCount { m_k }; downCount > 0; --downCount)
                 {
                     if (draw[downCount - 1] < m_n - 1)
                     {   
                         ++draw[downCount - 1];
-                        for (uint upCount { downCount }; upCount < m_k; ++upCount)
+                        for (int upCount { downCount }; upCount < m_k; ++upCount)
                         {
                             draw[upCount] = 0;
                         }
@@ -325,7 +326,7 @@ class UrnOR
         {   
             if(valid(draw) && draw != Draw(m_k,0))
             {
-                for(uint downCount {m_k}; downCount > 0; --downCount)
+                for(int downCount {m_k}; downCount > 0; --downCount)
                 {
                     if(draw[downCount-1] != 0)
                     {
@@ -342,22 +343,23 @@ class UrnOR
             throw std::underflow_error("There is no valid back draw.");
         }
 
-        virtual Draw draw(uint ordinalnumber) const
+        virtual Draw draw(int ordinalnumber) const
         {   
             Draw draw(m_k, 0);
-            if (!(ordinalnumber < m_z))
+            if (!(ordinalnumber < m_z) || ordinalnumber < 0)    //uint -> int
             {
-                throw std::overflow_error("There is no valid draw.");
+                //throw std::overflow_error("There is no valid draw.");
+                throw std::out_of_range("There is no valid draw for this ordinalnumber."); //
             }
 
             for (int posCount {static_cast<int>(m_k - 1)}; posCount >= 0; --posCount)
             {
-                for (uint timesCount {m_n - 1}; timesCount >= 1; --timesCount)
+                for (int timesCount {m_n - 1}; timesCount >= 1; --timesCount)
                 {
-                    if (static_cast<uint>(pow(m_n, posCount)) * timesCount <= ordinalnumber)
+                    if (static_cast<int>(pow(m_n, posCount)) * timesCount <= ordinalnumber)
                     {
                         draw[posCount] = timesCount;
-                        ordinalnumber -= static_cast<uint>(pow(m_n, posCount)) * timesCount;
+                        ordinalnumber -= static_cast<int>(pow(m_n, posCount)) * timesCount;
                         break;
                     }
                 }
@@ -379,31 +381,73 @@ class UrnOR
         virtual ~UrnOR() = default;
          
     protected:
-        const uint m_n,
-                   m_k,
-                   m_z;
+        const int m_n,
+                  m_k,
+                  m_z;
    };
 
 int main()
 {   
     UrnOR urn {2,2};
-    
+    vector<int> v {1,2,3,4};
+
+    auto it1 {urn.begin()};
+    auto it2 {urn.end()};
+
+    auto itv1 {v.begin()};
+    auto itv2 {v.end()};
+
+    cout << boolalpha << (it1 == it2) << endl;
+    cout << boolalpha << (it1 != it2) << endl;
+    cout << boolalpha << (it1 < it2) << endl;
+    cout << boolalpha << (it1 > it2) << endl;
+    cout << boolalpha << (it1 <= it2) << endl;
+    cout << boolalpha << (it1 >= it2) << endl;
+    cout << endl;
+    cout << boolalpha << (itv1 == itv2) << endl;
+    cout << boolalpha << (itv1 != itv2) << endl;
+    cout << boolalpha << (itv1 < itv2) << endl;
+    cout << boolalpha << (itv1 > itv2) << endl;
+    cout << boolalpha << (itv1 <= itv2) << endl;
+    cout << boolalpha << (itv1 >= itv2) << endl;
+
+    /*
     auto it {urn.begin()};
-    cout << it.ordinalnumber() << endl;
-    cout << it.stat() << endl;
-    it -= 1;
-    cout << it.ordinalnumber() << endl;
-    cout << it.stat() << endl;
-    it -= 1;
-    cout << it.ordinalnumber() << endl;
-    cout << it.stat() << endl;
-    it -= 1;
-    cout << it.ordinalnumber() << endl;
-    cout << it.stat() << endl;
-    it -= 1;
-    cout << it.ordinalnumber() << endl;
+    cout << to_string(it[2.6]) << endl;
+
+    auto itv {v.begin()};
+    cout << itv[2.3] << endl;
+
+
+    /*
+    int count = urn.end() - urn.begin();
+    int vcount = v.end() - v.begin();
     
+    int count = urn.rend() - urn.rbegin();
+    int vcount = v.rend() - v.rbegin();
+
+    int count = urn.rbegin() - urn.rend();
+    int vcount = v.rbegin() - v.rend();
     
+    int count = urn.begin() - urn.end();
+    int vcount = v.begin() - v.end();
+      
+    cout << count << endl;
+    cout << vcount << endl;
+
+
+    auto it {urn.begin()};
+    it -= 3;
+    int count = urn.end() - it;
+
+    auto itv {v.begin()};
+    itv -= 3;
+    int vcount = v.end() - itv;
+
+    cout << count << endl;
+    cout << vcount << endl;
+
+    /*
     cout << "+= und -=" << endl;
     auto it {urn.begin()};
     cout << to_string(*it) << endl;
